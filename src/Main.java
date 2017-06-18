69c129faa3c95650a97421a406e3413964317e4e
Pato285 <pisbej@gmail.com>
2017-06-18 14:46:28 -0300

    Add Password strength check

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import java.math.BigInteger;
 
+import com.devewm.pwdstrength.*;
 
 public class Main implements AuxFun{
 
     public static void main(String[] args){
         System.out.println(AuxFun.levenshteinDistance("hola","dsh"));
         System.out.println(AuxFun.maxSubsecuence("hola","dshoh"));
-        System.out.println(AuxFun.typesOfChars("hoA7!la"));
+        System.out.println(AuxFun.typesOfChars("hoA!la"));
+        
+        PasswordStrengthMeter passwordStrengthMeter = PasswordStrengthMeter.getInstance();
+        
+        BigInteger result = passwordStrengthMeter.iterationCount("aaaa");
+        System.out.println(result);
     }
 
 }
