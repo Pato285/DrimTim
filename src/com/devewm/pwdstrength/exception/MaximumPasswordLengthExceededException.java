12a78143c11ca7fbe62394fef58d9fe8bece40a8
Pato285 <pisbej@gmail.com>
2017-06-18 19:49:35 -0300

    Add base lab project

--- src/com/devewm/pwdstrength/exception/MaximumPasswordLengthExceededException.java
+++
@@ -1,8 +0,0 @@
-package com.devewm.pwdstrength.exception;
-
-public class MaximumPasswordLengthExceededException extends
-		UnsupportedOperationException {
-
-	private static final long serialVersionUID = 1697523826935659748L;
-
-}
